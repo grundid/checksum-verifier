4d899499b076527c8a6fb425bd1c6df89a903c09
Adrian Stabiszewski <as@grundid.de>
2012-03-30 18:15:59 +0200

    added a search for the original link with extract function for the data-checksum attribute

--- src/chrome/content/checksum.js
+++ src/chrome/content/checksum.js
@@ -23,29 +23,47 @@ var ChecksumExtension = {
 
 	downloadObserver : {
 		observe : function(aSubject, topic, aData) {
-			ChecksumExtension.log("observe: " + topic);
 			if (topic == "dl-done") {
 				var dl = aSubject.QueryInterface(Components.interfaces.nsIDownload);				
+				var linkInfo = ChecksumExtension.downloads[dl.targetFile.path];
+				ChecksumExtension.log("linkInfo: " + linkInfo.checksum);
+				if (linkInfo.checksum == null) {
+					var document = linkInfo.document;
+					var checksumMd5 = this.createChecksum(dl.targetFile,"MD5");
+					var checksumSha1 = this.createChecksum(dl.targetFile,"SHA1");
+					ChecksumExtension.log("MD5 for "+dl.displayName+" => " + checksumMd5);
+					ChecksumExtension.log("SHA1 for "+dl.displayName+" => " + checksumSha1);
+					this.findChecksumInDocument(document.getElementsByTagName("body"), [checksumMd5,checksumSha1]);
+				}
+				else {
+					var hashTypeAndValue = linkInfo.checksum.split(":");
+					if (hashTypeAndValue.length == 2) {
+						var actualChecksum = this.createChecksum(dl.targetFile,hashTypeAndValue[0].toUpperCase());
+						ChecksumExtension.log("verifing: "+dl.displayName+" => actual:" + actualChecksum+" expected: "+hashTypeAndValue[1]);
+						
+						if (actualChecksum != hashTypeAndValue[1]) {
+							this.notifyFailedCheck(actualChecksum,hashTypeAndValue[1],dl.displayName);
+						}
+					}
+				}
 
-				var checksumMd5 = this.createChecksum(dl.targetFile,"MD5");
-				var checksumSha1 = this.createChecksum(dl.targetFile,"SHA1");
-				ChecksumExtension.log("MD5 for "+dl.displayName+" => " + checksumMd5);
-				ChecksumExtension.log("SHA1 for "+dl.displayName+" => " + checksumSha1);
-
-// var checksumSha1 = this.createChecksum(dl.targetFile,"SHA1");
-// var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-// .getService(Components.interfaces.nsIPromptService);
-// prompts.alert(window, "Checksums", "MD5: "+checksumMd5+"\nSHA1:
-// "+checksumSha1);
-				
-				var document = ChecksumExtension.downloads[dl.targetFile.path];
-				this.findChecksumInDocument(document.getElementsByTagName("body"), [checksumMd5,checksumSha1]);
 				delete ChecksumExtension.downloads[dl.targetFile.path];
 			}
 			else if (topic == "dl-start") {
 				var dl = aSubject.QueryInterface(Components.interfaces.nsIDownload);				
-				ChecksumExtension.downloads[dl.targetFile.path] = content.document;
+				var linkInfo = {};
+				linkInfo["document"] = content.document;
+				var fullUrl = dl.source.prePath+dl.source.path;
+				linkInfo["checksum"] = this.findLinkChecksum(content.document.getElementsByTagName("a"), fullUrl);
+				ChecksumExtension.downloads[dl.targetFile.path] = linkInfo;
 			}
+		},
+		
+		notifyFailedCheck : function(actual, expected, fileName) {
+			var prompts = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+			 .getService(Components.interfaces.nsIPromptService);
+			 prompts.alert(window, "Checksum verification failed", "Checksum verification failed for "+fileName
+					 + "\nExpected: "+expected+"\nActual: "+actual);
 		},
 		
 		createChecksum : function(fileToCheck, hashType) {
@@ -57,7 +75,7 @@ var ChecksumExtension = {
 			// hash check
 			var ch = Components.classes["@mozilla.org/security/hash;1"]
 			.createInstance(Components.interfaces.nsICryptoHash);
-			ch.init(ch[hashType]);
+			ch.init(ch[hashType]); // initWithString
 			const PR_UINT32_MAX = 0xffffffff;
 			ch.updateFromStream(istream, PR_UINT32_MAX);
 			var hash = ch.finish(false);
@@ -83,6 +101,17 @@ var ChecksumExtension = {
 				else {
 					if (this.findChecksumInDocument(node.childNodes,checksums))
 						return true;
+				}
+			}
+		},
+		
+		findLinkChecksum : function(aTags, fullUrl) {
+			for (var x = 0; x < aTags.length;x++) {
+				var node = aTags[x];
+				var href = node.getAttribute("href");
+// ChecksumExtension.log("compare "+fullUrl+" => "+href);
+				if (fullUrl.indexOf(href) != -1) {	// TODO endsWith compare
+					return node.getAttribute("data-checksum");					
 				}
 			}
 		}
